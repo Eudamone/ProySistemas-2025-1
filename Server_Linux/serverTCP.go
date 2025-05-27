46a22e3f20c89b82d14f9d3b976304a2c73b77b6
Eudamone <daza.adrian09@gmail.com>
2025-05-26 23:44:01 -0500

    Desarrollo de interfaz para el cliente

---
+++ Server_Linux/serverTCP.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+)
+
+func main() {
+	proy := app.New()
+	window := proy.NewWindow("Proyecto Sistemas Operativos")
+	window.Resize(fyne.NewSize(800, 600))
+
+	title := canvas.NewText("Proyecto Sistemas Operativos 2025-1", color.White)
+	title.TextSize = 30
+	text1 := canvas.NewText("Comment 1", color.White)
+	text2 := canvas.NewText("Comment 2", color.White)
+
+	//Contenedor del titulo
+	contTitle := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), title, layout.NewSpacer())
+	contCode := container.New(layout.NewHBoxLayout(), text1, layout.NewSpacer(), text2)
+	//contReport := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), title, layout.NewSpacer())
+
+	// Contenedor principal
+	contMain := container.New(layout.NewVBoxLayout(), contTitle, contCode)
+
+	window.SetContent(contMain)
+	window.ShowAndRun()
+}
